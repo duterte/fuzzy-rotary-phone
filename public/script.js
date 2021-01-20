25018c97ec041ba0bd58f51c46650bbfa6f5976d
German Ochea <ochea.duterte@gmail.com>
2021-01-21 01:24:35 +0800

    complete prototype

--- public/script.js
+++ public/script.js
@@ -1,15 +1,110 @@
 const continueBtn = document.querySelector('#continue');
-const answer = document.querySelector('#answer');
-const dataList = document.querySelector('#data-list');
 const winPrompt = document.querySelector('.prompt');
+
+const formData = new FormData();
+
+formData.counter = 0;
+formData.addData = function (value, cb) {
+  console.log('counter:', this.counter);
+  this.append(`data${this.counter}`, value);
+  this.counter++;
+  cb();
+};
+
 continueBtn.addEventListener('click', continueBtnClicked);
+window.addEventListener('load', bodyLoad);
+
+function renderNewData(answer) {
+  const dataList = document.querySelector('#data-list');
+  const hardCodedText = `Answer: <b>${answer}</b>`;
+  const text = document.createElement('p');
+  text.innerHTML = hardCodedText;
+  dataList.appendChild(text);
+}
 
 function continueBtnClicked() {
+  const answer = document.querySelector('.prompt .answer');
   if (answer.value) {
-    const hardCodedText = `Hi my name is John Doe and I'm from <b>${answer.value}</b>`;
-    const text = document.createElement('p');
-    text.innerHTML = hardCodedText;
-    dataList.appendChild(text);
-    winPrompt.style.visibility = 'hidden';
+    printData(answer.value);
   }
+}
+
+function printData(value) {
+  renderNewData(value);
+  hidePrompt();
+  formData.addData(value, () => {
+    console.log('formData Counter:', formData.counter);
+    getRequest(formData.counter);
+  });
+}
+
+function showPrompt() {
+  winPrompt.classList.remove('hide');
+  winPrompt.classList.add('show');
+}
+
+function hidePrompt() {
+  winPrompt.classList.remove('show');
+  winPrompt.classList.add('hide');
+}
+
+function renderPrompt(object) {
+  const { question, type, index } = object;
+  const questionText = document.querySelector('#question-text');
+  const questiontype = document.querySelector('#question-type');
+  questionText.innerHTML = `${question}`;
+  questiontype.innerHTML = '';
+  if (type.toLowerCase() === 'inputtext') {
+    continueBtn.classList.remove('hide');
+    const inputText = document.createElement('input');
+    inputText.type = 'text';
+    inputText.className = 'answer';
+    inputText.id = `data${index}`;
+    inputText.style = `width: 100%; height: 31px;`;
+    questiontype.append(inputText);
+  } else if (type.toLowerCase() === 'boolean') {
+    continueBtn.classList.add('hide');
+    const groupElement = document.createElement('div');
+    const yesButton = document.createElement('span');
+    groupElement.style = `display: flex; justify-content: space-between; width:197px;`;
+    yesButton.className = 'page-button';
+    yesButton.id = 'yes';
+    yesButton.innerText = 'YES';
+    const noButton = document.createElement('span');
+    noButton.className = 'page-button';
+    noButton.id = 'no';
+    noButton.innerText = 'NO';
+    yesButton.addEventListener('click', () => printData('yes'));
+    noButton.addEventListener('click', () => printData('no'));
+    groupElement.append(noButton);
+    groupElement.append(yesButton);
+    questiontype.append(groupElement);
+  }
+  showPrompt();
+}
+
+function getRequest(param) {
+  const index = param;
+  fetch(`http://localhost:3000/questions/${index}`)
+    .then(res => {
+      if (res.status > 399) {
+        throw new Error(`Network Request failed status code ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(question => {
+      if (question.text) {
+        const payload = {
+          question: question.text,
+          type: question.type,
+          index: index,
+        };
+        renderPrompt(payload);
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+function bodyLoad() {
+  getRequest(1);
 }
--- server.js
+++ server.js
@@ -4,9 +4,38 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const questions = [
+  { text: 'Please state your address ?', type: 'InputText' },
+  { text: 'How much is the monthy rate ?', type: 'InputText' },
+  { text: 'Is this for sale ?', type: 'Boolean' },
+];
+
+app.use((req, res, next) => {
+  console.log('request received', req.url);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.resolve('public/index.html'));
 });
-app.use('/', express.static(path.resolve('public')));
 
+app.get('/questions/:index', (req, res) => {
+  try {
+    const { index } = req.params;
+    let payload = {};
+    if (questions.length >= parseInt(index)) {
+      payload = { ...questions[index] };
+    }
+    return res.status(200).json(payload);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
+});
+
+app.post('/', (req, res) => {
+  return res.status(200).json('Done.');
+});
+
+app.use('/', express.static(path.resolve('public')));
 app.listen(PORT, () => console.log('server is running'));
