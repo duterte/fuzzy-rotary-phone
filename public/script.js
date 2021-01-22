8a63daca366df7f1d06860e84b99a41e9c16b2dc
German Ochea <ochea.duterte@gmail.com>
2021-01-22 20:07:33 +0800

    Complete prototype

--- public/script.js
+++ public/script.js
@@ -1,284 +1,168 @@
 const continueBtn = document.querySelector('#continue');
-// const winPrompt = document.querySelector('.prompt');
 
-const formData = new FormData();
-let winPrompt = undefined;
+const store = [];
 
-formData.counter = 0;
-formData.addData = function (value, cb) {
-  this.append(`data${this.counter}`, value);
-  this.counter++;
-  const element = document.querySelector('#prompt');
-  setTimeout(() => {
-    document.body.removeChild(element);
-    getRequest(this.counter);
-  }, 900);
-};
+// let winPrompt = undefined;
+
+function printStyle(htmlElement, cssObject) {
+  for (const cssProperty in cssObject) {
+    htmlElement.style[cssProperty] = cssObject[cssProperty];
+  }
+}
 
 class MessagePrompt {
-  constructor({ headingText = 'prompt', width = 258 }) {
+  constructor({ headingText = 'prompt' }) {
     this.headingText = headingText;
-    this.width = `${width}px`;
   }
 
   border() {
     const element = document.createElement('div');
     element.id = 'prompt';
-    element.className = 'hide';
-    const style = {
-      display: 'grid',
-      gridTemplateRows: 'auto 1fr auto',
-      width: this.width,
-      backgroundColor: 'white',
-      boxSizing: 'border-box',
-      boxShadow: '0 0 5px rgba(0, 0, 0, 0.5)',
-      color: 'black',
-    };
-
-    this.printStyle(element, style);
     return element;
   }
 
   heading() {
     const element = document.createElement('h4');
     element.innerText = this.headingText;
-    const style = {
-      alignSelf: 'start',
-      gridRow: '1/2',
-      width: '100%',
-      backgroundColor: 'green',
-      color: 'white',
-      fontWeight: 'bold',
-      textAlign: 'center',
-      padding: '8px 0',
-    };
-
-    this.printStyle(element, style);
-
     return element;
-  }
-
-  printStyle(htmlElement, cssObject) {
-    for (const cssProperty in cssObject) {
-      htmlElement.style[cssProperty] = cssObject[cssProperty];
-    }
   }
 }
 
-class TextType extends MessagePrompt {
-  constructor({ headingText, questionText, width }) {
-    super({ headingText, width });
-    this.questionText = questionText;
-    this.value = '';
+window.addEventListener('load', () => {
+  getRequest(0);
+});
+
+class TextType {
+  constructor(question) {
+    this.question = question;
   }
 
-  prompt() {
-    const element = document.createElement('div');
-    const style = {
-      display: 'grid',
-      gridRow: '2/3',
-      alignItems: 'center',
-      width: '100%',
-      height: '100%',
-      padding: '0 1.5em',
-      marginTop: '3em',
-    };
-
-    this.printStyle(element, style);
-
-    element.append(this.question());
-    element.append(this.userInput());
-    return element;
-  }
-
-  question() {
+  renderQuestion() {
     const element = document.createElement('p');
-    element.innerText = this.questionText;
-    const style = {
-      fontWeight: 'bold',
-      textAligin: 'justify',
-    };
-    this.printStyle(element, style);
-
+    element.innerText = this.question;
     return element;
   }
 
-  userInput() {
+  renderInput() {
     const element = document.createElement('input');
-    // element.id = 'userinput'
     element.type = 'text';
-    const style = {
-      width: '100%',
-      height: '32px',
-    };
-
-    this.printStyle(element, style);
-
-    element.oninput = e => {
-      this.value = e.target.value;
-      console.log(this.value);
-    };
+    element.className = 'answer';
     return element;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.className = 'question';
+    element.append(this.renderQuestion());
+    element.append(this.renderInput());
+    return element;
+  }
+}
+
+class BooleanType {
+  constructor(question) {
+    this.question = question;
+  }
+  renderQuestion() {
+    const element = document.createElement('p');
+    element.innerText = this.question;
+    return element;
+  }
+  renderBoolean() {
+    const element = document.createElement('div');
+    const yesBtn = document.createElement('span');
+    const noBtn = document.createElement('span');
+    const input = document.createElement('input');
+    input.style.display = 'none';
+    input.type = 'checkbox';
+    input.id = 'answer';
+    yesBtn.className = 'page-button boolean-button';
+    noBtn.className = 'page-button boolean-button';
+
+    yesBtn.innerText = 'YES';
+    noBtn.innerText = 'NO';
+
+    element.append(noBtn);
+    element.append(yesBtn);
+    element.append(input);
+    return element;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.className = 'question';
+    element.append(this.renderQuestion());
+    element.append(this.renderBoolean());
+    return element;
+  }
+}
+
+// Inversion of Control
+class WinPrompt extends MessagePrompt {
+  constructor({ headingText, width, input }) {
+    super({ headingText, width });
+    this.input = input;
   }
 
   continueBtn() {
     const element = document.createElement('span');
-    const border = this.border();
-
-    element.className = 'page-button';
+    element.className = 'page-button continue-button';
     element.innerText = 'CONTINUE';
-    const style = {
-      justifySelf: 'end',
-      alignSelf: 'end',
-      gridRow: '3/4',
-      marginTop: '6em',
-      marginRight: '.5em',
-      marginBottom: '.5em',
-      backgroundColor: 'green',
-      padding: '8px 16px',
-      color: 'white',
-    };
-    this.printStyle(element, style);
+    element.addEventListener('click', clickHandler);
 
-    element.onclick = () => {
+    function clickHandler(e) {
       const prompt = document.querySelector('#prompt');
-      if (this.value) {
-        prompt.classList.add('hide');
-        formData.addData(this.value);
-      }
-    };
+      prompt.classList.add('hide');
+      const answers = document.querySelectorAll('.answer');
+      answers.forEach(answer => {
+        store[store.length - 1].answers.push({
+          question: store[store.length - 1].input[0].question,
+          answer: answer,
+        });
+        const str = answer.value.toString();
+        store[store.length - 1].output = store[store.length - 1].output.replace(
+          '@:input:',
+          `<span>${str}</span>`
+        );
+      });
 
-    return element;
-  }
-
-  render() {
-    const border = this.border();
-    const heading = this.heading();
-    const prompt = this.prompt();
-    const continueBtn = this.continueBtn();
-    border.append(heading);
-    border.append(prompt);
-    border.append(continueBtn);
-    return border;
-  }
-}
-
-class BooleanType extends MessagePrompt {
-  constructor({ headingText, questionText, width }) {
-    super({ headingText, width });
-    this.questionText = questionText;
-  }
-
-  prompt() {
-    const element = document.createElement('div');
-    const style = {
-      display: 'grid',
-      gridRow: '2/3',
-      alignItems: 'center',
-      width: '100%',
-      height: '100%',
-      padding: '3.8em 1.5em 0',
-    };
-
-    this.printStyle(element, style);
-
-    element.append(this.question());
-    element.append(this.wrapper());
-    return element;
-  }
-
-  question() {
-    const element = document.createElement('p');
-    element.innerText = this.questionText;
-    const style = {
-      fontWeight: 'bold',
-      textAligin: 'justify',
-    };
-    this.printStyle(element, style);
-
-    return element;
-  }
-
-  yesBtn() {
-    const element = document.createElement('span');
-    element.classList = 'page-button';
-    element.innerText = 'YES';
-
-    const style = {
-      padding: '9px 19px',
-      backgroundColor: 'green',
-      color: 'white',
-    };
-
-    this.printStyle(element, style);
-    return element;
-  }
-
-  noBtn() {
-    const element = document.createElement('span');
-    element.classList = 'page-button';
-    element.innerText = 'NO';
-
-    const style = {
-      padding: '9px 19px',
-      backgroundColor: 'green',
-      color: 'white',
-    };
-
-    this.printStyle(element, style);
-    return element;
-  }
-
-  wrapper() {
-    const element = document.createElement('div');
-    const style = {
-      display: 'flex',
-      justifyContent: 'space-between',
-      paddingBottom: '1.7em',
-      paddingTop: '5em',
-    };
-
-    const noBtn = this.noBtn();
-    const yesBtn = this.yesBtn();
-    this.printStyle(element, style);
-
-    element.append(noBtn);
-    element.append(yesBtn);
-    return element;
-  }
-
-  render() {
-    const border = this.border();
-    const heading = this.heading();
-    const prompt = this.prompt();
-    border.append(heading);
-    border.append(prompt);
-    return border;
-  }
-}
-
-class Editor {
-  constructor() {
-    //
-  }
-  border() {
-    const element = document.createElement('div');
-    const id = 'editor';
-    const style = {
-      padding: '0 5em',
-    };
-
-    return element;
-  }
-
-  printStyle(htmlElement, cssObject) {
-    for (const cssProperty in cssObject) {
-      htmlElement.style[cssProperty] = cssObject[cssProperty];
+      const editor = document.querySelector('#editor');
+      const div = document.createElement('div');
+      div.className = 'data';
+      div.innerHTML = `${store[store.length - 1].output}`;
+      editor.append(div);
+      setTimeout(() => {
+        document.body.removeChild(prompt);
+        getRequest(store.length);
+      }, 900);
     }
+
+    return element;
+  }
+
+  render() {
+    const div = document.createElement('div');
+    div.className = 'question-wrapper';
+
+    for (const entry of this.input) {
+      let prompt;
+      if (entry.type.toLowerCase() === 'texttype') {
+        prompt = new TextType(entry.question);
+      } else if (entry.type.toLowerCase() === 'booleantype') {
+        prompt = new BooleanType(entry.question);
+      }
+      div.append(prompt.render());
+    }
+
+    const border = this.border();
+    const heading = this.heading();
+
+    border.append(heading);
+    border.append(div);
+    border.append(this.continueBtn());
+    return border;
   }
 }
-
-window.addEventListener('load', () => getRequest(0));
 
 function getRequest(param) {
   const index = param;
@@ -289,18 +173,13 @@ function getRequest(param) {
       }
       return res.json();
     })
-    .then(({ type, text }) => {
-      if (text) {
-        if (type.toLowerCase() === 'texttype') {
-          winPrompt = new TextType({ questionText: text });
-        } else if (type.toLowerCase() === 'booleantype') {
-          winPrompt = new BooleanType({ questionText: text });
-        }
-        console.log(winPrompt);
+    .then(json => {
+      const { input, output } = json;
+      if (input && input.length > 0) {
+        const winPrompt = new WinPrompt({ input: input });
+        store.push(json);
+        console.log(store);
         document.body.append(winPrompt.render());
-        setTimeout(() => {
-          document.querySelector('#prompt').classList.remove('hide');
-        }, 500);
       }
     })
     .catch(err => console.log(err));
--- server.js
+++ server.js
@@ -1,13 +1,85 @@
 const express = require('express');
 const path = require('path');
+const { stringify } = require('querystring');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// @:input: - represent users input needs to be filled;
+
+// Data Structure
+
+// {
+//   input: [
+//     {
+//       question: String,
+//       type: String,
+//     },
+//   ];
+//   output: String;
+//   answers: []
+// }
+
 const questions = [
-  { text: 'Please state your address ?', type: 'TextType' },
-  { text: 'How much is the monthy rate ?', type: 'TextType' },
-  { text: 'Is this for sale ?', type: 'BooleanType' },
+  {
+    input: [
+      {
+        question: 'Agreements Date:',
+        type: 'TextType',
+      },
+    ],
+    output: `THIS AMENDED AND RESTATED REAL ESTATE LEASE (this “Lease”) is dated <b>@:input:</b> and entered into between the following:`,
+    answers: [],
+  },
+  //
+  {
+    input: [
+      {
+        question: 'Lessers Company Name:',
+        type: 'TextType',
+      },
+      {
+        question: 'Lesser Address:',
+        type: 'TextType',
+      },
+      {
+        question: 'Landlords Company Name: ',
+        type: 'TextType',
+      },
+      {
+        question: 'Landlords Address: ',
+        type: 'TextType',
+      },
+    ],
+    output: `
+    <div style="display: grid;
+    grid-template-columns: 1fr 1fr; 
+    text-indent: 0;
+    grid-column-gap: 4em;
+    ">
+      <div style="padding-right: 5em">
+        <p><b>@:input:</b></p>
+        <p>@:input:</p>
+        <br/>
+        <p>Country of Jurisdiction:</p>
+        <p>United States of America</p>
+        <br/>
+        <p>- hereinafter referred to as “Tenant” -</p>
+      </div>
+
+      <div style="padding-right: 5em">
+        <p><b>@:input:</b></p>
+        <p>@:input:</p>
+        <br/>
+        <p>Country of Jurisdiction:</p>
+        <p>United States of America</p>
+        <br/>
+        <p>- hereinafter referred to as “Landlord”</p>
+      </div>
+    </div>
+    `,
+    answers: [],
+  },
 ];
 
 app.use((req, res, next) => {
